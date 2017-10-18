079267dfa17047e75aee863337712023221ad0cb
supriya-premkumar <supriya@fastmail.com>
2017-10-18 16:30:21 -0700

    Retrofit dependencies commit

---
+++ app/src/main/java/edu/stanford/slac/powernetlab/Model/Powernet.java
@@ -0,0 +1,26 @@
+package edu.stanford.slac.powernetlab.Model;
+
+
+public class Powernet {
+    String device_name;
+    String device_status;
+    String device_load;
+    int device_id;
+
+    public String getDevice_name() {
+        return device_name;
+    }
+
+    public String getDevice_status() {
+        return device_status;
+    }
+
+    public String getDevice_load() {
+        return device_load;
+    }
+
+    public int getDevice_id() {
+        return device_id;
+    }
+
+}
